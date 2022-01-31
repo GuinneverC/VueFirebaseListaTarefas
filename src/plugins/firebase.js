61e113c958dcb6a3d25adda0f88f59262c927383
GuinneverC <guinnevercorrea.1@gmail.com>
2022-01-31 18:18:45 -0300

    vue com firebase

---
+++ src/plugins/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCS8KAeq2vsn7WH8Jk_tcNCc516Y_TF1lA",
+  authDomain: "listatarefasvue.firebaseapp.com",
+  projectId: "listatarefasvue",
+  storageBucket: "listatarefasvue.appspot.com",
+  messagingSenderId: "197283952860",
+  appId: "1:197283952860:web:e79df40df80493d3dedc19"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+const profileCollection = db.collection('profile')
+const tasksCollection = db.collection('tasks')
+
+export {
+    db, auth, profileCollection, tasksCollection
+}
---
+++ src/router/index.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import { auth } from '../plugins/firebase'
+
+Vue.use(VueRouter)
+
+const routes = [
+
+  {
+    path: '/',
+    component: () => import('@/layouts/default'),
+    meta: {
+      requiresAuth: true
+    },
+    children: [
+       {
+    path: '',
+    name: 'Home',
+    component: Home
+  },
+  
+  {
+    path: '/perfil',
+    name: 'Perfil',
+    component: () => import(/* webpackChunkName: "perfil" */ '../views/perfil.vue')
+  },
+    ]
+  },
+  {
+    path: '/',
+    component: () => import('@/layouts/blank'),
+    children: [
+      {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/login.vue')
+  }
+    ]
+  },
+  
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+if (requiresAuth && !auth.currentUser) {
+  next('/login')
+} else {
+  next()
+}
+})
+
+
+export default router
